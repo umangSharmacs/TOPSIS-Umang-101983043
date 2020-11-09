0c463a04ea513fee97abd6d2b7c0acf2b585536e
umangSharmacs <umang.s@hotmail.co.uk>
2020-11-09 18:29:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'TOPSIS-Umang-101983043',         # How you named your package folder (MyLib)
   packages = ['TOPSIS-Umang-101983043'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple python program to implement Multi Decision Criteria using TOPSIS',   # Give a short description about your library
   author = 'Umang Sharma',                   # Type in your name
   author_email = 'umangsharma.cs@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/umangSharmacs/TOPSIS-Umang-101983043',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/umangSharmacs/TOPSIS-Umang-101983043/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/umangSharmacs/TOPSIS-Umang-101983043/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['SIMPLE', 'CONCISE', 'TOPSIS', 'MULTI DECISION CRITERIA', 'DATASCIENCE'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
